754623ea5e58bb1fe75fcb16a466d2e5efe388ae
Milosz <milosz.domagalaki@gmail.com>
2025-08-16 05:47:00 +0200

    created a get and post point

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("Hello World")
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel, EmailStr
+from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from typing import List
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    password: str
+    age: int
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/users/", response_model=List[UserCreate])
+def read_users(db: Session = Depends(get_db)):
+    users = db.query(models.Users).all()  # Pobieramy wszystkie rekordy z tabeli
+    return users
+
+@app.post("/users/")
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = models.Users(
+        name=user.name,
+        surname=user.surname,
+        email=user.email,
+        password=user.password,
+        age=user.age 
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
